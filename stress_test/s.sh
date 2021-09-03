bd0e38550a7c5efefd6d394ced3ffefcf47aac1f
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2021-09-03 18:01:37 +0530

    yo

--- stress_test/s.sh
+++
@@ -1,9 +0,0 @@
-for((i = 1; ; ++i)); do
-    # run bash s.sh
-    echo $i
-    ./gen $i > int
-    # ./a < int > out1
-    # ./brute < int > out2
-    # diff -w out1 out2 || break
-    diff -w <(./a < int) <(./brute < int) || break
-done
